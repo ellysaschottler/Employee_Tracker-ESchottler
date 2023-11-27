8eb4e9700adc787d9e74cb804a00ad23e2cb45f2
ellysaschottler <ellysastork@gmail.com>
2023-11-27 10:52:22 -0600

    corrected schema and wrote seeds

--- db/schema.sql
+++ db/schema.sql
@@ -21,8 +21,8 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id, INT
-  manager_id INT
+  manager_id INT,
+  role_id INT,
   FOREIGN KEY (role_id)
   REFERENCES role(id)
 );
