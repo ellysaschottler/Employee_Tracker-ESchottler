4710a0286cb4716e4938dfeea6983ddcc6f68c23
ellysaschottler <ellysastork@gmail.com>
2023-11-30 15:03:17 -0600

    reformatted functions to async await and fixed bugs

--- index.js
+++ index.js
@@ -1,282 +1,240 @@
 const inquirer =  require('inquirer');
-const mysql = require('mysql2')
+const mysql = require('mysql2/promise')
 const cTable = require('console.table')
 
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'employee_db'
-    },
-    console.log('Connected to employee_db')
-);
+let db = null
 
-function init() {
-    inquirer.prompt(questionMainMenu).then((answers) => {
-      
-        if (answers = "View All Employees"){
-            printAllEmployees()
+async function init() {
+    db = await mysql.createConnection(
+        {
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'employee_db'
+        })
+
+    const questionMainMenu =[
+        {
+            type: 'list',
+            name: 'mainMenu',
+            message: 'What would you like to do?',
+            choices:
+            [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add a Role',
+                'View All Departments', 
+                'Add Department',
+                'Quit',
+            ]
+        },
+    ]
+    
+    while (true) {
+        const answers = await inquirer.prompt(questionMainMenu)
+        const answer = answers.mainMenu
+
+        if (answer == "View All Employees") {
+            await printAllEmployees()
+        } else if (answer == "Add Employee") {
+            await promptAddEmpl(answers)
+        } else if (answer == "Update Employee Role") {
+            await promptUpdateRole(answers)
+        } else if (answer == "View All Roles") {
+            await printAllRoles()
+        } else if (answer == "Add a Role") {
+            await promptAddRole(answers)
+        } else if (answer == "View All Departments") {
+            await printAllDepartments()
+        } else if (answer == "Add Department") {
+            await promptAddDept(answers)
+        } else if (answer == "Quit") {
+            break;
         }
-        if (answers = "Add Employee"){
-            addEmployee()
-        }
-        if (answers = "Update Employee Role"){
-           promptUpdateRole()
-        }
-        if (answers = "View All Roles"){
-            printAllRoles()
-        }
-        if (answers = "Add a Role"){
-            promptAddRole()
-        }
-        if (answers = "View All Departments"){
-            printAllDepartments()
-        }
-        if (answers = "Add Department"){
-            promptAddDept()
-        }
-        if (answers = "Quit"){
-           
-        }        
-    });
+    }
+
+    // close the connection pool
+    db.end()
 }
 
-//Initialize the app
-init()
-
-
-
-const questionMainMenu =[
-    {
-        type: 'list',
-        name: 'mainMenu',
-        message: 'What would you like to do?',
-        choices:
-        [
-            'View All Employees',
-            'Add Employee',
-            'Update Employee Role',
-            'View All Roles',
-            'Add a Role',
-            'View All Departments', 
-            'Add Department',
-            'Quit',
-        ]
-    },
-]
-
-const questionAddDept = [
-    {
-        type: 'input',
-        name: 'deptName',
-        message: 'What is the name of the department?',
-        
-    },
-]
-
-const questionAddRole = [
-    {
-        type: 'input',
-        name: 'roleChoice',
-        message: 'What is the name of the role?',
-    },
-    {
-        type: 'input',
-        name: 'roleSalary',
-        message: 'What is the salary of the role?',
-    },
-    {
-        type: 'list',
-        name: 'roleDept',
-        message: 'Which department does the role belong to?',
-        choices: listAllDepartments()
-    },
-]
-
-const questionUpdateRole = [
-   { 
-        type: 'list',
-        name: 'employeeChoice',
-        message: 'Which employee you like to update?',
-        choices: listEmployeeFullName()
-    },
-    { 
-        type: 'list',
-        name: 'roleChoice',
-        message: 'What is the new role?',
-        choices: listAllRoles()
-    },
-    
-]
-const questionAddEmpl = [
-    {
-        type: 'input',
-        name: 'emplFirstName',
-        message: "What is the employee's first name?",
-    },
-    {
-        type: 'input',
-        name: 'emplLastName',
-        message: "What is the employee's last name?",
-    },
-    {
-        type: 'list',
-        name: 'emplRole',
-        message: "What is the employee's role?",
-        choices: listAllRoles()
-    },
-    {
-        type: 'list',
-        name: 'emplManager',
-        message: "Who is the employee's manager?",
-        choices: listManagerFullName()
-    }
-]
 
 
 //Sub Question prompts
 
-function promptAddEmpl(){
-    inquirer.prompt(questionAddEmpl).then((answers) => {
-        addEmployee()
-    })
+async function promptAddEmpl(){
+    const questionAddEmpl = [
+        {
+            type: 'input',
+            name: 'emplFirstName',
+            message: "What is the employee's first name?",
+        },
+        {
+            type: 'input',
+            name: 'emplLastName',
+            message: "What is the employee's last name?",
+        },
+        {
+            type: 'list',
+            name: 'emplRole',
+            message: "What is the employee's role?",
+            choices: await listAllRoleTitles()
+        },
+        {
+            type: 'list',
+            name: 'emplManager',
+            message: "Who is the employee's manager?",
+            choices: await listManagerFullName()
+        }
+    ]
+
+    const subAnswers = await inquirer.prompt(questionAddEmpl)
+    await addEmployee(subAnswers)
 }   
 
+async function promptUpdateRole() {
+    const questionUpdateRole = [
+        { 
+             type: 'list',
+             name: 'employeeChoice',
+             message: 'Which employee you like to update?',
+             choices: await listEmployeeFullName()
+         },
+         { 
+             type: 'list',
+             name: 'roleChoice',
+             message: 'What is the new role?',
+             choices: await listAllRoleTitles()
+         },
+         
+    ]
 
-function promptUpdateRole() {
-    inquirer.prompt(questionUpdateRole).then((answers) => {
-        updateEmployeeRole()
-    })
+    const subAnswers = inquirer.prompt(questionUpdateRole)
+    await updateEmployeeRole(subAnswers)
 }
-function promptAddRole(){
-    inquirer.prompt(questionAddRole).then((answers) => {
-        addRole()            
-    })   
+
+async function promptAddRole(){
+    const questionAddRole = [
+        {
+            type: 'input',
+            name: 'roleChoice',
+            message: 'What is the name of the role?',
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary of the role?',
+        },
+        {
+            type: 'list',
+            name: 'roleDept',
+            message: 'Which department does the role belong to?',
+            choices: await listAllDepartments()
+        },
+    ]
+
+    const subAnswers = inquirer.prompt(questionAddRole)
+    await addRole(subAnswers)
 }
-function promptAddDept() {
-    inquirer.prompt(questionAddDept).then((answers) => {
-        addDepartment()
-    }) 
+async function promptAddDept() {
+    const questionAddDept = [
+        {
+            type: 'input',
+            name: 'deptName',
+            message: 'What is the name of the department?',
+        },
+    ]
+
+    const subAnswers = await inquirer.prompt(questionAddDept)
+    await addDepartment(subAnswers)
 }
 
 //Query functions
-function printAllEmployees(){
-    db.query("SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name , ' ', m.last_name) AS manager FROM employee AS e INNER JOIN role AS r ON e.role_id = r.id INNER JOIN department AS d ON d.id = r.department_id LEFT JOIN employee AS m ON e.manager_id = m.id", 
-    function (err, results) {
-        console.table(results)
-    }) 
+async function printAllEmployees(){
+    const [rows, fields] = await db.execute("SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name , ' ', m.last_name) AS manager FROM employee AS e INNER JOIN role AS r ON e.role_id = r.id INNER JOIN department AS d ON d.id = r.department_id LEFT JOIN employee AS m ON e.manager_id = m.id")
+    console.table(rows)
 }
 
-function printAllRoles() {
-    db.query("SELECT title FROM role",
-    function (err, results) {
-        console.table(results)
-    })
+async function printAllRoles() {
+    const roles = await listAllRoles()
+    console.table(roles)
 }
 
-function listAllRoles() {
-    db.query("SELECT title FROM role",
-    function (err, results) {
-        return
-    })
+async function listAllRoles() {
+    const [rows, fields] = await db.execute("SELECT title FROM role")
+    return rows
 }
 
-function printAllDepartments() {
-    db.query("SELECT name FROM department",
-    function(err, results) {
-        console.table(results)
-    })
-}
-function listAllDepartments() {
-    db.query("SELECT name FROM department",
-    function(err, results) {
-        return
-    })
-}
-function listEmployeeFullName(){
-    db.query("SELECT CONCAT(employee.first_name , ' ', employee.last_name) AS full_name FROM employee",
-    function(err, results) {
-        return
-    })
-}
-function listManagerFullName(){
-    db.query("SELECT CONCAT(m.first_name , ' ', m.last_name) AS manager FROM employee AS e LEFT JOIN employee AS m ON e.manager_id = m.id WHERE m.first_name IS NOT NULL",
-    function (err, results) {
-        return
-    })
+async function listAllRoleTitles() {
+    return (await listAllRoles()).map(role => role.title)
 }
 
-function getManagerIDfromName () {
-    var splitManager = answers.manager.split(" ")
-    var managerFirstName = splitManager[0]
-    var managerLastName = splitManager [1]
-    db.query("SELECT id FROM employee WHERE first_name = ? AND last_name = ?",
-     [managerFirstName, managerLastName],
-     function(err, results) {
-        return
-     })
-}
-function getRoleIDfromRoleTitle() {
-    db.query("SELECT id FROM role WHERE name = ?",
-    answers.roleChoice,
-    function(err, results) {
-        return
-    })
+async function printAllDepartments() {
+    const departments = await listAllDepartments()
+    console.table(departments)
 }
 
-function addEmployee() {
-    managerID = getManagerIDfromName()
-    roleID = getRoleIDfromRoleTitle()
-    db.query("INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ?, ?, ?, ?",
-    [answers.emplFirstName, answers.emplLastName, managerID, roleID],
-    function (err, results) {
-        console.log("This employee has been added.")
-    }) 
+async function listAllDepartments() {
+    const [rows, fields] = await db.execute("SELECT name FROM department")
+    return rows
 }
 
-function getEmployeeIDfromName() {
-    var splitEmployee = answers.employeeChoice.split(" ")
-    var employeeFirstName = splitEmployee[0]
-    var employeeLastName = splitEmployee[1]
-    db.query("SELECT id FROM employee WHERE first_name = ? AND last_name = ?",
-    [employeeFirstName, employeeLastName],
-    function(err, results) {
-        return
-    })
+async function listEmployeeFullName(){
+    const [rows, fields] = await db.execute("SELECT CONCAT(employee.first_name , ' ', employee.last_name) AS full_name FROM employee")
+    return rows.map(row => row.full_name)
 }
 
-function updateEmployeeRole() {
-    var employeeID = getEmployeeIDfromName()
-    var roleID = getRoleIDfromRoleTitle()
-    db.query("UPDATE employee SET role_id = ? WHERE employee.id = ?",
-    [roleID, employeeID],
-    function (err, results) {
-        (console.log("This employee has been updated"))
-    })
+async function listManagerFullName(){
+    const [rows, fields] = await db.execute("SELECT DISTINCT CONCAT(m.first_name , ' ', m.last_name) AS manager FROM employee AS e LEFT JOIN employee AS m ON e.manager_id = m.id WHERE m.first_name IS NOT NULL")
+    return rows.map(row => row.manager)
 }
 
-function getDepartmentIDFromName() {
-    db.query("SELECT id FROM department WHERE name = ?",
-    answers.deptName,
-    function(err, results) {
-        return
-    })
+async function getEmployeeIDFromName (managerName) {
+    const [rows, fields] = await db.execute("SELECT id FROM employee WHERE CONCAT(first_name , ' ', last_name) = ?", [managerName])
+    return rows[0].id
 }
 
-function addRole() {
+async function getRoleIDFromTitle(roleTitle) {
+    const [rows, fields] = await db.execute("SELECT id FROM role WHERE title = ?", [roleTitle])
+    return rows[0].id
+}
+
+async function addEmployee(subAnswers) {
+    managerID = await getEmployeeIDFromName(subAnswers.emplManager)
+    roleID = await getRoleIDFromTitle(subAnswers.emplRole)
+    await db.execute(
+        "INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES (?, ?, ?, ?)",
+        [subAnswers.emplFirstName, subAnswers.emplLastName, managerID, roleID])
+
+    console.log("This employee has been added.")
+}
+
+async function updateEmployeeRole(subAnswers) {
+    var employeeID = await getEmployeeIDFromName(subAnswers.employeeChoice)
+    var roleID = await getRoleIDFromTitle(subAnswers.roleChoice)
+    await db.execute("UPDATE employee SET role_id = ? WHERE employee.id = ?", [roleID, employeeID])
+
+    console.log("This employee has been updated")
+}
+
+function getDepartmentIDFromName(subAnswers) {
+    return db.execute("SELECT id FROM department WHERE name = ?", [subAnswers.deptName])
+}
+
+async function addRole(subAnswers) {
     var deptID = getDepartmentIDFromName()
-    db.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
-    [answers.roleName, answers.roleSalary, deptID],
-    function(err, results) {
-        console.log("This role has been added.")
-    })    
+    await db.execute(
+        "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+        [subAnswers.roleName, subAnswers.roleSalary, deptID])
+    
+    console.log("This role has been added.")
 }
 
-function addDepartment() {
-    db.query("INSERT INTO department (name) VALUES (?)",
-     answers.deptName,
-     function(err, results) {
-        console.log("The department has been added.")
-     })
+async function addDepartment(subAnswers) {
+    await db.execute("INSERT INTO department (name) VALUES (?)", [subAnswers.deptName])
+
+    console.log("The department has been added.")
 }
+
+init()
